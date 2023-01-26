4c8a27479a706bc88ba065a739d9496387901960
marta <mlazaro.ieu2021@student.ie.edu>
2023-01-26 10:59:47 +0100

    this is the third session class example

---
+++ print_examples.py
@@ -0,0 +1,4 @@
+print("This is a text",end="|I want this at the end|\n")
+print("this is another")
+print("this",2+2,"be","written","like","this")
+print("this",2+2,"be","written","like","this",sep="!b ?!")
